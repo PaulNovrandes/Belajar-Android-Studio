ca92da9f380a5467df701cd09399d114dc1766ce
paul novrandes <novrandes.tanuma@gmail.com>
2021-06-01 17:09:44 +0700

    Add files via upload

---
+++ DroidCafe2/app/src/main/java/android/example/droidcafepart1/MainActivity.java
@@ -0,0 +1,120 @@
+package android.example.droidcafepart1;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.ui.AppBarConfiguration;
+
+import android.example.droidcafepart1.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_order) {
+//            return true;
+//        }
+
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+                return true;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                return true;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                return true;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                return true;
+            default:
+                // Do nothing
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * show a message that the donut image was clicked.
+     */
+    public void showDonutOrder(View view){
+        showFoodOrder(getString(R.string.donut_order_message));
+    }
+
+    /**
+     * show a message that the ice cream sandwich image was clicked.
+     */
+    public void showIceCreamOrder(View view){
+        showFoodOrder(getString(R.string.ice_cream_order_message));
+    }
+
+    /**
+     * show a message that the foryo image was clicked.
+     */
+
+    public void showFroyoOrder(View view){
+        showFoodOrder(getString(R.string.froyo_order_message));
+    }
+
+    /**
+     * Display a Toast message for the food order and start the OrderActivity activity.
+     *
+     * @param message Message to display.
+     */
+    public void showFoodOrder(String message) {
+        displayToast(message);
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
